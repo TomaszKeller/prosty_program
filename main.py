b7664e7decb959068b03ed3e80119feef4d68d96
TomaszKeller <kellertomek@gmail.com>
2021-09-18 08:43:18 +0200

    przelicznik walut

---
+++ main.py
@@ -0,0 +1,15 @@
+class Currency_converter:
+
+    def pln_to_eu(selfself, pln_value):
+        return int(pln_value) / 4.59
+
+    def eu_to_pln(selfself, eu_value):
+        return int(eu_value) + 4.59
+
+    def pln_to_usd(selfself, pln_value):
+        return int(pln_value) / 3.92
+
+    def usd_to_pln(selfself, usd_value):
+        return  int(usd_value) * 3.92
+
+choice
